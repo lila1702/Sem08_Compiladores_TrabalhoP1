1c1bf38b8e14e1af160bcaac70e5388d911ba681
Lila M. <lila.msfrazao@gmail.com>
2024-12-10 21:21:44 -0300

    Update consts

--- src/consts.py
+++ src/consts.py
@@ -4,13 +4,16 @@ class Consts:
     # Tokens
     DIGITOS = '0123456789'
     LETRAS = string.ascii_letters
-    LETRAS_DIGITOS = DIGITOS + LETRAS
-
+    LETRAS_DIGITOS = DIGITOS + LETRAS   
+    
     PERSONAGEM = 'personagem'
     CENARIO    = 'cenario'
     ACAO       = 'acao'
     ITEM       = 'item'
-
+    VARIAVEL   = 'variavel'
+    ESCOLHA    = 'escolha'
+    OPCAO      = 'opcao'
+    CONDICAO   = 'condicao' 
     # Símbolos utilizados
     LBRACE     = '{'
     RBRACE     = '}'
@@ -19,13 +22,34 @@ class Consts:
     COMMA      = ','
     STRING     = 'STRING'
     INT        = 'INT'
-
+    FLOAT      = 'FLOAT'
+    BOOLEAN    = 'BOOLEAN'
+    ASSIGN     = '='
     EOF       = '$EOF'
-
+    
+    # Para comparações
+    IGUAL      = '=='
+    DIFERENTE  = '!='
+    MENOR      = '<'
+    MAIOR      = '>'
+    MENORIGUAL = '<='
+    MAIORIGUAL = '>='   
     # Palavras reservadas
     KEYS = [
         PERSONAGEM,
         CENARIO,
         ACAO,
-        ITEM
+        ITEM,
+        VARIAVEL,
+        ESCOLHA,
+        OPCAO,
+        CONDICAO
+    ]
+    
+    # Tipos suportados para as variáveis
+    TIPOS = [
+        STRING,
+        INT,
+        FLOAT,
+        BOOLEAN
     ]
