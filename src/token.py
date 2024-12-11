1e84a50286589fed1386c6ac4e49105d4c7f2d3d
Lila M. <lila.msfrazao@gmail.com>
2024-12-10 21:47:34 -0300

    Update removal

--- src/token.py
+++
@@ -1,9 +0,0 @@
-class Token:
-    def __init__(self, tipo, valor=None):
-        self.tipo = tipo
-        self.valor = valor
-        
-    def __repr__(self):
-        if (self.valor is not None):
-            return f"Token({self.tipo}, {repr(self.valor)})"
-        return f"Token({self.tipo})"
